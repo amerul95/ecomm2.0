f46996d4aca62a9b4fcb960d7e20e99610eda428
Polygot <alimiamerul@gmail.com>
2024-06-25 02:01:37 +0800

    start popular product

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Megasale } from '../components/megasale/Megasale.js';
+import { PopularProduct } from '../components/popularproduct/PopularProduct.js';
 
 export default function home() {
   return (
     <div>
       <Megasale/>
+      <PopularProduct/>
     </div>
   )
 }
---
+++ src/components/popularproduct/PopularProduct.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const PopularProduct = () => {
+  return (
+    <div className='max-w-7xl px-2 mx-auto sm:px-6 lg:px-8 m-2 flex my-4 md:my-8 '>
+        <h2 className='text-3xl font-semibold'>POPULAR PRODUCTS</h2>
+    </div>
+  )
+}
