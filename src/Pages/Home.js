9c310558233eaa87f7f79d6139831bb11b1d4aac
amerul95 <120275262+amerul95@users.noreply.github.com>
2024-06-25 16:16:56 +0800

    finish subscribe

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Megasale } from '../components/megasale/Megasale.js';
 import { PopularProduct } from '../components/popularproduct/PopularProduct.js';
+import { Subscribe } from '../components/subscribe/Subscribe.js';
 
 export default function home() {
   return (
     <div>
       <Megasale/>
       <PopularProduct/>
+      <Subscribe/>
     </div>
   )
 }
